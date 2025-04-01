e266c09971ae9f70a69610ab293a969e2d4b1910
BOB <BOB@example.com>
2025-04-01 12:06:11 +0200

    Añado funcion

---
+++ my_function.m
@@ -0,0 +1,13 @@
+function my_function(x)
+    % This function takes an input x and returns its square
+    % Input:
+    %   x - a number
+    % Output:
+    %   y - the square of x
+
+    % Calculate the square of x
+    y = x^2;
+
+    % Display the result
+    fprintf('The square of %d is %d\n', x, y);
+end
