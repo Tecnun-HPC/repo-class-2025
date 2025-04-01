421485f95f89eba0a41f6337a3912a10d0aaa74d
BOB <BOB@example.com>
2025-04-01 12:08:05 +0200

    Cambios de bob en 2 fichreos.

--- my_function.m
+++ my_function.m
@@ -10,4 +10,17 @@ function my_function(x)
 
     % Display the result
     fprintf('The square of %d is %d\n', x, y);
+    disp('asdf');
+    % Calcula factorial de x
+    if x < 0
+        error('Input must be a non-negative integer');
+    end
+    if x == 0
+        y = 1;
+    else
+        y = 1;
+        for i = 1:x
+            y = y * i;
+        end
+    end
 end
--- my_script.m
+++ my_script.m
@@ -1,3 +1,5 @@
 disp('Mi script');
 a = 666; % Alice ha cambiado
 disp('Soy BOB');
+% Hacer factorial de 6
+b = my_function(6);
